2098536c94b9d3473b8200d45746f41d34d3cc3e
momtysamby <momtysamby@gmail.com>
2020-12-17 22:30:31 +0300

    Lecture 125

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class BurgerBuilder extends Component{
+    render () {
+        return(
+            <React.Fragment>
+                <div>Burger</div>
+                <div>Div Controls</div>
+            </React.Fragment>
+        )
+    }
+}
+
+export default BurgerBuilder
