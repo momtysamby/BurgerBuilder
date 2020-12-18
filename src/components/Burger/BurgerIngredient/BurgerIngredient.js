ec09bfbbfc70cc170fe13a46b0315484de52cd59
momtysamby <momtysamby@gmail.com>
2020-12-18 15:47:15 +0300

    Lecture 127

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { Component } from 'react'
 import classes from './BurgerIngredient.css'
+import PropTypes from 'prop-types'
 
-const burgerIngredient = (props) => {
-    let ingredient = null
-    switch (props.type) {
+class BurgerIngredient extends Component {
+    render() {
+        let ingredient = null
+    switch (this.props.type) {
         case ('bread-bottom'):
             ingredient = <div className={classes.BreadBottom}></div>
             break
@@ -37,6 +39,12 @@ const burgerIngredient = (props) => {
             ingredient = null    
     }
     return ingredient    
+
+    }
+}    
+
+BurgerIngredient.propTypes = {
+    type: PropTypes.string.isRequired
 }
 
-export default burgerIngredient
+export default BurgerIngredient
