a8a5333a3323d3d0f3a66edba7e141b86dd0ba30
momtysamby <momtysamby@gmail.com>
2020-12-19 18:44:44 +0300

    Lecture 130

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,18 @@ import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <BurgerIngredient key={igKey + i} type={igKey}/>
             }) //Lecture 129
-        }) 
+        }).reduce((arr, el) => {
+            return arr.concat(el)
+        }, []) //Lecture 130
+        console.log(transformedIngredients)
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please add something</p>
+    }
 
     return(
         <div className={classes.Burger}>
