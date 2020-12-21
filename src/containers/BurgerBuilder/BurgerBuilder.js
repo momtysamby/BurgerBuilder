ddbc817b4fdeb38a0e44933b0cab6a2c674e5a43
momtysamby <momtysamby@gmail.com>
2020-12-21 09:20:23 +0300

    Lecture 132

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Burger from '../../components/Burger/Burger'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 
 class BurgerBuilder extends Component{
     state = {
@@ -15,6 +16,7 @@ class BurgerBuilder extends Component{
         return(
             <React.Fragment>
                 <Burger ingredients={this.state.ingredients}/>
+                <BuildControls />
             </React.Fragment>
         )
     }
